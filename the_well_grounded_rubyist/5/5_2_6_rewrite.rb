906441596b53b7309051b4c43c548f7e9413acb2
wyattsweet <wyattsweet@gmail.com>
2016-01-26 11:00:41 -0800

    ch5

---
+++ the_well_grounded_rubyist/5/5_2_6_rewrite.rb
@@ -0,0 +1,16 @@
+class Car
+  @@makes = []
+  @@cars = []
+  attr_reader :make
+
+  def self.total_count
+    @total_count ||= 0
+  end
+
+  def self.total_count=(n)
+    @total_count = n
+  end
+
+end
+
+
